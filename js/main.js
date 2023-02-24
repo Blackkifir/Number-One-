5ee4f17b6e884dc8379f401b99ee8c68bdfba26f
black-kifir <example@gmail.com>
2023-02-24 15:44:41 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+'use strict'
+
+//Массив 
+// let arr = ['orange', 'Black', 'white', 'red', 5];
+// console.log(arr[3]);
+
+// свойство Confirm
+// let result = confirm('Are you hear?');
+// alert('Yes');
+
+// свойство Prompt
+// let result = prompt('Как вас зовут?', 'Николай'); 
+// console.log(result);
+
+// console.log(typeof result);
+
+
+// Массив который выводит в сайт текст и спанит вопросы
+// let answers = [];
+// answers [0] = prompt('Сколько вам лет');
+// answers [2] = prompt('Сколько стоит машина?');
+// answers [3] = prompt('Где вЫ есть?');
+
+// document.write(answers);
+
+
+// Следующее занятие ===============2
+
+// Полезный навык доллара:)
+// let user = 10;
+// alert(`Вызов значения ${user}`);
+
+
+//Next  LESSON
+
+
+//операторы!
+
+// Оператор &&
+// let user = 'Привет';
+// let player = 'Пока';
+
+// console.log(player && user);
+
+
